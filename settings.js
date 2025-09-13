2c4a06f61eb6fb527987e93dbfc6cf2b10b80b1d
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-09-13 05:32:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,54 +6,24 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "NENO-XMD~yXgnjRDS#8CBE9zrbAEFB5mlHqcHmd9JtJ0fyBef8b4vWykyYmx8",
-    // add your Session Id make sure it starts with NENO~
-
-    PREFIX: process.env.PREFIX || ".",
-    // add your prefix for bot
-
+    SESSION_ID: process.env.SESSION_ID,
+    PREFIX: process.env.PREFIX,
     BOT_NAME: process.env.BOT_NAME || "ɴᴇɴᴏ-xᴍᴅ",
-    // add bot name here for menu
-
-    MODE: process.env.MODE || "public",
-    // make bot public-private-inbox-group 
-
-    LINK_WHITELIST: "youtube.com,github.com",
-
-    LINK_WARN_LIMIT: 3, // Number of warnings before action
-
-    LINK_ACTION: "kick", // "kick", "mute", or "none"
-
-    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-    // make true or false status auto seen
-
-    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    // make true if you want auto reply on status 
-
-    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-    // make true if you want auto reply on status 
-
+    MODE: process.env.MODE,
+    LINK_WHITELIST: process.env.LINK_WHITELIST,
+    LINK_WARN_LIMIT: Number(process.env.LINK_WARN_LIMIT),
+    LINK_ACTION: process.env.LINK_ACTION,
+    AUTO_STATUS_SEEN: convertToBool(process.env.AUTO_STATUS_SEEN),
     AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY NENO.XMD 💖😍*",
-    // set the auto reply message on status reply  
-
-    WELCOME: process.env.WELCOME || "true",
-    // true if want welcome and goodbye msg in groups 
-
-    ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-    // make true to know who dismiss or promoted a member in group
-
-    ANTI_LINK: process.env.ANTI_LINK || "true",
-    // make anti link true,false for groups 
-
-    MENTION_REPLY: process.env.MENTION_REPLY || "false",
-    // make true if want auto voice reply if someone mention you 
-
+    AUTO_STATUS_REACT: convertToBool(process.env.AUTO_STATUS_REACT),
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG,
+    WELCOME: convertToBool(process.env.WELCOME),
+    ADMIN_EVENTS: convertToBool(process.env.ADMIN_EVENTS),
+    ANTI_LINK: convertToBool(process.env.ANTI_LINK),
+    MENTION_REPLY: convertToBool(process.env.MENTION_REPLY),
     MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/n8g2o7.jpg",
-    // add custom menu and mention reply image url
-
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/pf7ytb.jpg",
     // add img for alive msg
-
     LIVE_MSG: process.env.LIVE_MSG || 
 `> ʙᴏᴛ ɪs sᴘᴀʀᴋɪɴɢ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ᴀʟɪᴠᴇ
 
@@ -63,56 +33,44 @@ module.exports = {
 
 > ɢɪᴛʜᴜʙ : https://github.com/Nimeshkamihiran`,
     // add alive msg here 
-
-    STICKER_NAME: process.env.STICKER_NAME || "ɴᴇɴᴏ ᴠ3",
-    // type sticker pack name 
-
-    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-    // make this true for custom emoji react  
-
-    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,💛,💚,💙,💜,🤎,🖤,🤍",
-    // choose custom react emojis by yourself 
-
-    DELETE_LINKS: process.env.DELETE_LINKS || "false",
-    // automatic delete links without removing member 
-
+    STICKER_NAME: process.env.STICKER_NAME,
+    CUSTOM_REACT: convertToBool(process.env.CUSTOM_REACT),
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS.split(','),
+    DELETE_LINKS: convertToBool(process.env.DELETE_LINKS),
     OWNER_NUMBER: process.env.OWNER_NUMBER || "94721584279",
-    // add your bot owner number
-
     OWNER_NAME: process.env.OWNER_NAME || "ɴɪᴍᴇꜱʜᴋᴀ࿐",
-    // add bot owner name
-
     DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴɪᴍᴇꜱʜᴋᴀ ᴍɪʜɪʀᴀᴍ*",
-    // add bot owner description  
+    READ_MESSAGE: convertToBool(process.env.READ_MESSAGE),
+    AUTO_REACT: convertToBool(process.env.AUTO_REACT),
+    ANTI_BAD: convertToBool(process.env.ANTI_BAD),
+    ANTI_LINK_KICK: convertToBool(process.env.ANTI_LINK_KICK),
+    AUTO_STICKER: convertToBool(process.env.AUTO_STICKER),
+    AUTO_REPLY: convertToBool(process.env.AUTO_REPLY),
+    ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE),
+    PUBLIC_MODE: convertToBool(process.env.PUBLIC_MODE),
+    AUTO_TYPING: convertToBool(process.env.AUTO_TYPING),
+    READ_CMD: convertToBool(process.env.READ_CMD),
+    DEV: process.env.DEV,
+    ANTI_VV: convertToBool(process.env.ANTI_VV),
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH,
+    AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING),
+    version: process.env.version || "0.0.5",
+    START_MSG: process.env.START_MSG || 
+`*Hᴇʟʟᴏ ᴀʟʟ ɪᴀᴍ ɴᴇɴᴏ xᴍᴅ ᴡʜᴀᴛꜱᴘᴘ ʙᴏᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ! 👋🏻* 
 
-    READ_MESSAGE: process.env.READ_MESSAGE || "false",
-    // Turn true or false for automatic read msgs
+*💖ᴋᴇᴇᴘ ᴏɴ ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ💫☺️* 
 
-    AUTO_REACT: process.env.AUTO_REACT || "false",
-    // make this true or false for auto react on all msgs
+> sᴜʙsᴄʀɪʙᴇ ʏᴛ ᴄʜᴀɴɴᴇʟ ғᴏʀ ᴛᴜᴛᴏʀɪᴀʟs
+https://youtube.com/@MihirangaMihiranga-k1k
 
-    ANTI_BAD: process.env.ANTI_BAD || "false",
-    // false or true for anti bad words  
+- *ʏᴏᴜʀ ʙᴏᴛ ᴘʀᴇғɪx: ➡️[ . ]*
+> - ʏᴏᴜ ᴄᴀɴ ᴄʜᴀɴɢᴇ ᴜʀ ᴘʀᴇғɪx ᴜsɪɴɢ ᴛʜᴇ .ᴘʀᴇғɪx ᴄᴏᴍᴍᴀɴᴅ
 
-    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-    // make anti link true,false for groups 
+> ᴅᴏɴᴛ ғᴏʀɢᴇᴛ ᴛᴏ sʜᴀʀᴇ, sᴛᴀʀ & ғᴏʀᴋ ᴛʜᴇ ʀᴇᴘᴏ ⬇️ 
+https://github.com/Nimeshkamihiran/neno-xmd-V3
 
-    AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    // make true for automatic stickers 
-
-    AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    // make true or false automatic text reply 
-
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-    // make true for always online 
-
-    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
-    // make false if want private mode
-
-    AUTO_TYPING: process.env.AUTO_TYPING || "false",
-    // true for automatic show typing  
-
-    READ_CMD: process.env.READ_CMD || "false",
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴɪᴍᴇꜱʜᴋᴀ ᴍɪʜɪʀᴀɴ💖😍`
+};    READ_CMD: process.env.READ_CMD || "false",
     // true if want mark commands as read  
 
     DEV: process.env.DEV || "263780166288",
